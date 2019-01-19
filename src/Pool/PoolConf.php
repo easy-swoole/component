998b77b6e5af0a404f5a201b76744ea5a7827918
yf <291323003@qq.com>
2019-01-19 22:06:18 +0800

    修改默认等待时间

--- src/Pool/PoolConf.php
+++ src/Pool/PoolConf.php
@@ -16,7 +16,7 @@ class PoolConf
     protected $maxIdleTime = 15;
     protected $maxObjectNum = 20;
     protected $minObjectNum = 5;
-    protected $getObjectTimeout = 0.5;
+    protected $getObjectTimeout = 3.0;
 
     protected $extraConf = [];
 
