6f71f270c0448bed9335804c020eca8e89e234cb
yf <291323003@qq.com>
2019-02-15 13:41:52 +0800

    pool 合理性优化

--- src/Pool/TraitInvoker.php
+++ src/Pool/TraitInvoker.php
@@ -9,8 +9,10 @@
 namespace EasySwoole\Component\Pool;
 
 
+use EasySwoole\Component\Context\ContextManager;
 use EasySwoole\Component\Pool\Exception\PoolEmpty;
 use EasySwoole\Component\Pool\Exception\PoolException;
+use Swoole\Coroutine;
 
 trait TraitInvoker
 {
@@ -33,6 +35,31 @@ trait TraitInvoker
             }
         }else{
             throw new PoolException(static::class." convert to pool error");
+        }
+    }
+
+    public static function defer($timeout = null)
+    {
+        $key = md5(static::class);
+        $obj = ContextManager::getInstance()->get($key);
+        if($obj){
+            return $obj;
+        }else{
+            $pool = PoolManager::getInstance()->getPool(static::class);
+            if($pool instanceof AbstractPool){
+                $obj = $pool->getObj($timeout);
+                if($obj){
+                    Coroutine::defer(function ()use($pool,$obj){
+                        $pool->recycleObj($obj);
+                    });
+                    ContextManager::getInstance()->set($key,$obj);
+                    return $obj;
+                }else{
+                    throw new PoolEmpty(static::class." pool is empty");
+                }
+            }else{
+                throw new PoolException(static::class." convert to pool error");
+            }
         }
     }
 }
