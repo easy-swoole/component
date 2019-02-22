2e7135b05fcd2c932054b70d32cce7d6535852c0
Tioncico <31308307+tioncico@users.noreply.github.com>
2019-02-22 11:08:43 +0800

    修复当定时器name为数字时无法clear的bug

--- src/Timer.php
+++ src/Timer.php
@@ -16,51 +16,52 @@ class Timer
     protected $timerList = [];
     protected $timerMap = [];
 
-    function loop(int $ms,callable $callback,$name = null):int
+    function loop(int $ms, callable $callback, $name = null): int
     {
-        $id =  swoole_timer_tick($ms, $callback);
+        $id = swoole_timer_tick($ms, $callback);
         $this->timerList[$id] = $id;
-        if($name !== null){
+        if ($name !== null) {
             $this->timerMap[md5($name)] = $id;
         }
         return $id;
     }
 
-    function clear($timerIdOrName):bool
+    function clear($timerIdOrName): bool
     {
-        if(is_numeric($timerIdOrName)){
-            if(!isset($this->timerList[$timerIdOrName])){
-                return false;
-            }
-            swoole_timer_clear($timerIdOrName);
-            $key = array_search($timerIdOrName,$this->timerMap);
-            if($key !== null){
-                unset($this->timerMap[$key]);
-            }
-            return true;
-        }else{
-            $timerIdOrName = md5($timerIdOrName);
-            if(!isset($this->timerMap[$timerIdOrName])){
-                return false;
-            }
-            $id = $this->timerMap[$timerIdOrName];
-            swoole_timer_clear($id);
-            unset($this->timerList[$id]);
-            unset($this->timerMap[$timerIdOrName]);
-            return true;
+        if (!isset($this->timerMap[md5($timerIdOrName)]) && !isset($this->timerList[$timerIdOrName])) {
+            return false;
         }
+        if (is_numeric($timerIdOrName)) {
+            if (isset($this->timerList[$timerIdOrName])) {
+                swoole_timer_clear($timerIdOrName);
+                $key = array_search($timerIdOrName, $this->timerMap);
+                if ($key !== null) {
+                    unset($this->timerMap[$key]);
+                }
+                return true;
+            }
+        }
+        $timerIdOrName = md5($timerIdOrName);
+        if (!isset($this->timerMap[$timerIdOrName])) {
+            return false;
+        }
+        $id = $this->timerMap[$timerIdOrName];
+        swoole_timer_clear($id);
+        unset($this->timerList[$id]);
+        unset($this->timerMap[$timerIdOrName]);
+        return true;
     }
 
-    function clearAll():bool
+    function clearAll(): bool
     {
-        foreach ($this->timerList as $id){
+        foreach ($this->timerList as $id) {
             swoole_timer_clear($id);
         }
         $this->timerList = [];
         return true;
     }
 
-    function after(int $ms,callable $callback):int
+    function after(int $ms, callable $callback): int
     {
         return swoole_timer_after($ms, $callback);
     }
