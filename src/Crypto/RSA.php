3b8d48106c1a9e511b2123311f70201539cbf6ed
yf <291323003@qq.com>
2019-07-30 23:16:58 +0800

    up

--- src/Crypto/RSA.php
+++ src/Crypto/RSA.php
@@ -6,5 +6,11 @@ namespace EasySwoole\Component\Crypto;
 
 class RSA
 {
-
+    protected $publicKey;
+    protected $privateKey;
+    public function __construct($publicKey,$privateKey)
+    {
+        $this->publicKey = openssl_get_publickey($publicKey);
+        $this->privateKey = openssl_get_privatekey($privateKey);
+    }
 }
