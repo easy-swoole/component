f911efae8e77d4b4aacbe869e8696c9ff527391c
如果的如果 <admin@fosuss.com>
2020-03-19 13:51:05 +0800

    up fix

--- src/ChannelLock.php
+++ src/ChannelLock.php
@@ -10,29 +10,42 @@ use Swoole\Coroutine\Channel;
 class ChannelLock
 {
     protected $list;
+    protected $status = [];
     use Singleton;
 
     function lock(string $lockName,float $timeout = -1):bool
     {
+        $cid = Coroutine::getCid();
+        if(isset($this->status[$cid])){
+            return true;
+        }
         if(!isset($this->list[$lockName])){
             $this->list[$lockName] = new Channel(1);
         }
         /** @var Channel $channel */
         $channel = $this->list[$lockName];
-        return $channel->push(1,$timeout);
+        $ret = $channel->push(1,$timeout);
+        $this->status[$cid] = true;
+        return true;
     }
 
     function unlock(string $lockName,float $timeout = -1):bool
     {
+        $cid = Coroutine::getCid();
+        if(!isset($this->status[$cid])){
+            return true;
+        }
         if(!isset($this->list[$lockName])){
             $this->list[$lockName] = new Channel(1);
         }
         /** @var Channel $channel */
         $channel = $this->list[$lockName];
         if($channel->isEmpty()){
+            unset($this->status[$cid]);
             return true;
         }else{
             $channel->pop($timeout);
+            unset($this->status[$cid]);
             return true;
         }
     }
@@ -42,7 +55,7 @@ class ChannelLock
         $lock = $this->lock($lockName,$timeout);
         if($lock){
             Coroutine::defer(function ()use($lockName){
-               $this->unlock($lockName);
+                $this->unlock($lockName);
             });
         }
         return $lock;
