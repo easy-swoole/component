23d8fc8a90e55541d56a61ab4f7be5924a1fa180
如果的如果 <admin@fosuss.com>
2020-08-10 11:56:40 +0800

    完善wait group

--- src/WaitGroup.php
+++ src/WaitGroup.php
@@ -9,12 +9,15 @@ use Swoole\Coroutine\Channel;
 class WaitGroup
 {
     private $count = 0;
-    private $chan;
+    /** @var Channel  */
+    private $channel;
     private $success = 0;
+    private $size;
 
-    public function __construct(int $size = 8)
+    public function __construct(int $size = 128)
     {
-        $this->chan = new Channel($size);
+        $this->size = $size;
+        $this->reset();
     }
 
     public function add()
@@ -29,7 +32,7 @@ class WaitGroup
 
     public function done()
     {
-        $this->chan->push(1);
+        $this->channel->push(1);
     }
 
     public function wait(?float $timeout = 15)
@@ -42,12 +45,34 @@ class WaitGroup
         while(($this->count > 0) && ($left > 0))
         {
             $start = round(microtime(true),3);
-            if($this->chan->pop($left) === 1)
+            if($this->channel->pop($left) === 1)
             {
                 $this->count--;
                 $this->success++;
             }
             $left = $left - (round(microtime(true),3) - $start);
         }
+    }
+
+
+    function reset()
+    {
+        $this->close();
+        $this->count = 0;
+        $this->success = 0;
+        $this->channel = new Channel($this->size);
+    }
+
+    function close()
+    {
+        if($this->channel){
+            $this->channel->close();
+            $this->channel = null;
+        }
+    }
+
+    function __destruct()
+    {
+        $this->close();
     }
 }
