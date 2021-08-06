fee0a85a2930f7cc82027e26859ef870605b6565
stitch <gaobinzhan@gmail.com>
2021-08-06 16:39:45 +0800

    Add: Manager Helpr

--- src/Process/Manager.php
+++ src/Process/Manager.php
@@ -12,102 +12,115 @@ class Manager
 {
     use Singleton;
 
+    /** @var AbstractProcess[] $processList */
     protected $processList = [];
     protected $autoRegister = [];
     protected $table;
 
 
-    function __construct()
+    public function __construct()
     {
         $this->table = new Table(2048);
-        $this->table->column('pid',Table::TYPE_INT,8);
-        $this->table->column('name',Table::TYPE_STRING,50);
-        $this->table->column('group',Table::TYPE_STRING,50);
-        $this->table->column('memoryUsage',Table::TYPE_INT,8);
-        $this->table->column('memoryPeakUsage',Table::TYPE_INT,8);
-        $this->table->column('startUpTime',Table::TYPE_INT,8);
+        $this->table->column('pid', Table::TYPE_INT, 8);
+        $this->table->column('name', Table::TYPE_STRING, 50);
+        $this->table->column('group', Table::TYPE_STRING, 50);
+        $this->table->column('memoryUsage', Table::TYPE_INT, 8);
+        $this->table->column('memoryPeakUsage', Table::TYPE_INT, 8);
+        $this->table->column('startUpTime', Table::TYPE_INT, 8);
         $this->table->create();
     }
 
-    function getProcessTable():Table
+    public function getProcessTable(): Table
     {
         return $this->table;
     }
 
-    function getProcessByPid(int $pid):?AbstractProcess
+    public function getProcessByPid(int $pid): ?AbstractProcess
     {
-        /** @var AbstractProcess $p */
-        foreach ($this->processList as $p)
-        {
-            if($p->getPid() === $pid){
-                return  $p;
+        foreach ($this->processList as $process) {
+            if ($process->getPid() === $pid) {
+                return $process;
             }
         }
         return null;
     }
 
-    function getProcessByName(string $name):array
+    public function getProcessByName(string $name): array
     {
-        //可能存在同名进程，因此是返回数组。
+        $ret = [];
+        foreach ($this->processList as $process) {
+            if ($process->getProcessName() === $name) {
+                $ret[] = $process;
+            }
+        }
+
+        return $ret;
     }
 
-    function getProcessByGroup():array
+    public function getProcessByGroup(string $group): array
     {
+        $ret = [];
+        foreach ($this->processList as $process) {
+            if ($process->getConfig()->getProcessGroup() === $group) {
+                $ret[] = $process;
+            }
+        }
 
+        return $ret;
     }
 
-    function kill($pidOrGroupName,$sig = SIGTERM):array
+    public function kill($pidOrGroupName, $sig = SIGTERM): array
     {
         $list = [];
-        if(is_numeric($pidOrGroupName)){
+        if (is_numeric($pidOrGroupName)) {
             $info = $this->table->get($pidOrGroupName);
-            if($info){
+            if ($info) {
                 $list[$pidOrGroupName] = $pidOrGroupName;
             }
-        }else{
-            foreach ($this->table as $key => $value){
-                if($value['group'] == $pidOrGroupName){
+        } else {
+            foreach ($this->table as $key => $value) {
+                if ($value['group'] == $pidOrGroupName) {
                     $list[$key] = $value;
                 }
             }
         }
         $this->clearPid($list);
-        foreach ($list as $pid => $value){
-            Process::kill($pid,$sig);
+        foreach ($list as $pid => $value) {
+            Process::kill($pid, $sig);
         }
         return $list;
     }
 
-    function info($pidOrGroupName = null)
+    public function info($pidOrGroupName = null)
     {
         $list = [];
-        if($pidOrGroupName == null){
-            foreach ($this->table as $pid =>$value){
+        if ($pidOrGroupName == null) {
+            foreach ($this->table as $pid => $value) {
                 $list[$pid] = $value;
             }
-        }else if(is_numeric($pidOrGroupName)){
+        } else if (is_numeric($pidOrGroupName)) {
             $info = $this->table->get($pidOrGroupName);
-            if($info){
+            if ($info) {
                 $list[$pidOrGroupName] = $info;
             }
-        }else{
-            foreach ($this->table as $key => $value){
-                if($value['group'] == $pidOrGroupName){
+        } else {
+            foreach ($this->table as $key => $value) {
+                if ($value['group'] == $pidOrGroupName) {
                     $list[$key] = $value;
                 }
             }
         }
 
-        $sort = array_column($list,'group');
-        array_multisort($sort,SORT_DESC,$list);
-        foreach ($list as $key => $value){
+        $sort = array_column($list, 'group');
+        array_multisort($sort, SORT_DESC, $list);
+        foreach ($list as $key => $value) {
             unset($list[$key]);
             $list[$value['pid']] = $value;
         }
         return $this->clearPid($list);
     }
 
-    function addProcess(AbstractProcess $process,bool $autoRegister = true): Manager
+    public function addProcess(AbstractProcess $process, bool $autoRegister = true): Manager
     {
         $hash = spl_object_hash($process->getProcess());
         $this->autoRegister[$hash] = $autoRegister;
@@ -115,12 +128,10 @@ class Manager
         return $this;
     }
 
-    function attachToServer(Server $server)
+    public function attachToServer(Server $server)
     {
-        /** @var AbstractProcess $process */
-        foreach ($this->processList as $hash => $process)
-        {
-            if($this->autoRegister[$hash] === true){
+        foreach ($this->processList as $hash => $process) {
+            if ($this->autoRegister[$hash] === true) {
                 $server->addProcess($process->getProcess());
             }
         }
@@ -128,13 +139,13 @@ class Manager
 
     public function pidExist(int $pid)
     {
-        return Process::kill($pid,0);
+        return Process::kill($pid, 0);
     }
 
     protected function clearPid(array $list)
     {
-        foreach ($list as $pid => $value){
-            if(!$this->pidExist($pid)){
+        foreach ($list as $pid => $value) {
+            if (!$this->pidExist($pid)) {
                 $this->table->del($pid);
                 unset($list[$pid]);
             }
